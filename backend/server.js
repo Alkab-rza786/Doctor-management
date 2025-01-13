3d9c93c030babcd28cdfeb57599625eb4e0566f4
Alkab Rza <153321032+Alkab-rza786@users.noreply.github.com>
2025-01-14 01:05:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 
 const corsOptions = {
   
-  origin: ['https://doctor-management-landinpage.onrender.com', 'http://localhost:5174'], // Your frontend URL
+  origin: ['https://doctor-management-landinpage.onrender.com', 'https://doctor-management-adminpanel.onrender.com'], // Your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'token','aToken','dToken'], 
   credentials: true,
